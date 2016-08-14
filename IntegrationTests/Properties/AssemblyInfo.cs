8ebb2cdc0930466b5155e715f23aecc3d75234ac
cbakk <cbakk@DESKTOP-T2AMP5A>
2016-08-14 16:38:05 +0200

    Naming

--- IntegrationTests/Properties/AssemblyInfo.cs
+++ IntegrationTests/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BlinkStickDotNet Intergration Tests")]
+[assembly: AssemblyTitle("BlinkStickDotNet Integration Tests")]
 [assembly: AssemblyDescription("Test your BlinkStick with these tests.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KeesTalksTech")]
